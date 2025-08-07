25c6cfbd8411ea275b69acd7245d0d83e3d7dc6a
Will Dean <wd60622@gmail.com>
2025-08-07 12:30:09 -0400

    auto import what is available

--- src/frame_search/__init__.py
+++ src/frame_search/__init__.py
@@ -1,6 +1,13 @@
-import narwhals as nw  #
+import importlib.util
 
 from frame_search.search import parse_search_query, create_search
+
+
+if importlib.util.find_spec("pandas") is not None:
+    import frame_search.pandas  # noqa: F401
+
+if importlib.util.find_spec("polars") is not None:
+    import frame_search.polars  # noqa: F401
 
 
 __all__ = [
